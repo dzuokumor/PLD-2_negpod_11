023c43b41cb12bba9ab6cecea76b7f63565bb38b
hubertmucyo <h.mucyo@alustudent.com>
2023-11-28 13:05:35 -0800

    fix

--- main.py
+++ main.py
@@ -4,28 +4,6 @@ print("Welcome to BodyMath: A smart BMI calculator")
 print("This application is designed to assess Body-Mass-Index, a common metrix used for gauging body weight in relation to height\n")
 
 print("\n1. Add patient\n2. Enter your details\n3. Display patient\n4. Medical advice\n5. Save and Exit")
-        choice = input("Enter your choice (1-5): ")
-            if choice == 1:
-                self.name = input("Enter your name: ")
-                self.gender = input("Enter your gender: ")
-                self.blood_type = input("Enter your blood type (O, A, B, AB): ").lower()
-                self.age = input("Enter your age: ")
-            elif choice == 2:
-                weight = float(input("Enter your weight (kg): "))
-                height = float(input("Enter your height (m): "))
-                bmi, category = bmi_calculator(weight, height)
-                print(f"Your BMI is {bmi:.2f}, and your category is {category}")
-            elif choice == 3:
-                self.display_patient_info()
-            elif choice == 4:
-                advice = dietary_advise(category, self.blood_type, return_advice=True)
-                print(advice)
-            elif choice == 5:
-                self.save_to_file(bmi, category, advice)
-                print("Exiting the application. Goodbye!")
-                break
-            else:
-                print("Invalid choice. Please enter a number between 1 and 5.")
 
 def bmi_calculator(weight, height):
     bmi = weight / (height ** 2)
