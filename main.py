b9e3473f6c714d1c84f60fb805445a9e61470680
KhotKeys <gabrielkhotgarangpawuoi@gmail.com>
2023-11-28 11:53:30 -0800

    Friends_are_family

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 print("Welcome to BodyMath: A smart BMI calculator")
 print("This application is designed to assess Body-Mass-Index, a common metrix used for gauging body weight in relation to height")
+print("\n1. Add patient\n2. Enter your details\n3. Display patient\n4. Medical advice\n5. Save and Exit")
+            choice = input("Enter your choice (1-5): ")
