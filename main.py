ae2151035fa5be791e36bdb0f2798294cc66b7d7
Mgrace1 <g.munezero1@alustudent.com>
2023-11-28 12:03:18 -0800

    Add-main

--- main.py
+++ main.py
@@ -2,3 +2,17 @@ print("Welcome to BodyMath: A smart BMI calculator")
 print("This application is designed to assess Body-Mass-Index, a common metrix used for gauging body weight in relation to height")
 print("\n1. Add patient\n2. Enter your details\n3. Display patient\n4. Medical advice\n5. Save and Exit")
             choice = input("Enter your choice (1-5): ")
+
+def bmi_calculator(weight, height):
+    bmi = weight / (height ** 2)
+
+    if bmi < 18.5:
+        category = "Underweight"
+    elif 18.5 <= bmi < 25:
+        category = "Normal weight"
+    elif 25 <= bmi < 30:
+        category = "Overweight"
+    else:
+        category = "Obese"
+
+    return bmi, category
