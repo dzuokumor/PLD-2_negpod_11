41d742b95e98f5962586d0f75fb4cc9625c5a2df
KhotKeys <gabrielkhotgarangpawuoi@gmail.com>
2023-11-28 11:50:26 -0800

    lit

--- intro.py
+++
@@ -1,2 +0,0 @@
-print("Welcome to BodyMath: A smart BMI calculator")
-print("This application is designed to assess Body-Mass-Index, a common metrix used for gauging body weight in relation to height")
