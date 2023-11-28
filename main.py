27cb5cc178c0dfac6f398eee5d38229e90d6798a
hubertmucyo <h.mucyo@alustudent.com>
2023-11-28 13:06:35 -0800

    fix

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ print("Welcome to BodyMath: A smart BMI calculator")
 print("This application is designed to assess Body-Mass-Index, a common metrix used for gauging body weight in relation to height\n")
 
 print("\n1. Add patient\n2. Enter your details\n3. Display patient\n4. Medical advice\n5. Save and Exit")
+choice = input("Enter your choice (1-5): ")
 
 def bmi_calculator(weight, height):
     bmi = weight / (height ** 2)
