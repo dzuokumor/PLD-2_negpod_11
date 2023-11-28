b509e157f8ebecf5de2851d140548f29b1663873
hubertmucyo <h.mucyo@alustudent.com>
2023-11-28 12:57:31 -0800

    line

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 print("Welcome to BodyMath: A smart BMI calculator")
 print("This application is designed to assess Body-Mass-Index, a common metrix used for gauging body weight in relation to height")
 print("\n1. Add patient\n2. Enter your details\n3. Display patient\n4. Medical advice\n5. Save and Exit")
