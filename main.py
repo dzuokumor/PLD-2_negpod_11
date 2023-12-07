c73279a7c76e2b66b66d4fbaf55fd14be31cab1f
Mucyo Hubert <144014119+hubertmucyo@users.noreply.github.com>
2023-12-08 01:15:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -85,13 +85,7 @@ class Patient:
             ");"
             )
             cursor.execute(create_report_table_query)
-            insert_data_query = ("INSERT INTO patient (name, gender, blood_type, age) VALUES (%s, %s, %s, %s);")
-            data = (self.name, self.gender, self.blood_type, self.age)
-            cursor.execute(insert_data_query, data)
-            connection.commit()
-        finally: 
-            cursor.close()
-            connection.close()
+      
     
         while True:
             print("\n1. Add patient\n2. Enter your details for BMI calculation\n3. Display patient information\n4. Medical advice\n5. Dietary advice\n6. Access your exercise routine\n7. Meet professional personnels and nutritionists\n8. Save and Exit")
@@ -128,11 +122,17 @@ class Patient:
                 print(doctor)
             elif choice == "8":
                 self.save_to_file(bmi, category, advice)
+                insert_data_query = ("INSERT INTO patient (name, gender, blood_type, age) VALUES (%s, %s, %s, %s);")
+                data = (self.name, self.gender, self.blood_type, self.age)
+                cursor.execute(insert_data_query, data)
+                connection.commit()
                 print("Exiting the application. Goodbye!")
                 break
             else:
                 print("Invalid choice. Please enter a number between 1 and 8.")
-
+          finally: 
+            cursor.close()
+            connection.close()
 def bmi_calculator(weight, height):
     bmi = weight / (height ** 2)
 
